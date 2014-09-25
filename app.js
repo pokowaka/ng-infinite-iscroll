082b401b644c58e2d29c04a8317c39f877737372
Erwin Jansen <pokowaka@gmail.com>
2014-09-24 17:45:45 -0700

    Fix to work on github.io

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ config(['$routeProvider', '$locationProvider', '$compileProvider', function($rou
 	@toc 1.
 	*/
 	$locationProvider.html5Mode(false);		//can't use this with github pages / if don't have access to the server
-	
+
 	// var staticPath ='/';
 	var staticPath;
 	// staticPath ='/angular-directives/ng-infinite-iscroll/';		//local
-	staticPath ='/';		//nodejs (local)
-	// staticPath ='/ng-infinite-iscroll/';		//gh-pages
+  //	staticPath ='/';		//nodejs (local)
+	staticPath ='/ng-infinite-iscroll/';		//gh-pages
 	var appPathRoute ='/';
 	var pagesPath =staticPath+'pages/';
-	
-	
+
+
 	$routeProvider.when(appPathRoute+'home', {templateUrl: pagesPath+'home/home.html'});
 	$routeProvider.otherwise({redirectTo: appPathRoute+'home'});
-	
+
 }]);
